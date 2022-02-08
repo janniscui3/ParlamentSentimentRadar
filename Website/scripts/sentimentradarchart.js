3a31494f059bcb85a7874917a5f34aa5ede627ba
TimoE <s7020028@stud.uni-frankfurt.de>
2022-02-08 12:03:37 +0100

    Added the rest of the charts

--- Website/scripts/sentimentradarchart.js
+++ Website/scripts/sentimentradarchart.js
@@ -4,50 +4,56 @@
 
 //##################################################
 
-// Token Line Chart
+// Sentiment Radar Chart
 var ctx3 = document.getElementById("sentiment_radar_chart");
-var tokenlinechart = new Chart(ctx3, {
-    type: 'radar',
-    data: {
-        labels: [
-            'Positiv',
-            'Neutral',
-            'Negativ'
-          ],
-        datasets: [{
+var sentimentradarchart = new Chart(ctx3, {
+  type: 'radar',
+  data: {
+      labels: [
+          'Positiv',
+          'Neutral',
+          'Negativ'
+        ],
+      datasets: [{
 
-            label: 'My Second Dataset',
-            data: [28, 48, 40],
-            fill: true,
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgb(54, 162, 235)',
-            pointBackgroundColor: 'rgb(54, 162, 235)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgb(54, 162, 235)'
-        }],
-        
+          label: 'My Second Dataset',
+          data: [28, 48, 40],
+          fill: true,
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgb(54, 162, 235)',
+          pointBackgroundColor: 'rgb(54, 162, 235)',
+          pointBorderColor: '#fff',
+          pointHoverBackgroundColor: '#fff',
+          pointHoverBorderColor: 'rgb(54, 162, 235)'
+      }],
+      
 
 
-    },
-    options: {
-        maintainAspectRatio: false,
-       
-     
-        layout: {
-            padding: {
-            left: 10,
-            right: 10,
-            top: 0,
-            bottom: -40
-            }
-        },
-        elements: {
+  },
+  options: {
+      maintainAspectRatio: false,
+      
+    
+      layout: {
+          padding: {
+          left: 10,
+          right: 10,
+          top: 0,
+          bottom: -40
+          }
+      },
+
+      legend: {
+        display: false
+      },
+
+      elements: {
         line: {
           borderWidth: 3
         }
       }
-    },
+  },
+    
   
 });
 
