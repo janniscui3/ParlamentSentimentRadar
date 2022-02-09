19bb030e81eca9d5bc49562078db72cac05beb24
muzisama1 <janniscui@gmail.com>
2022-02-09 22:47:15 +0100

    excluded conflicting servlet api's - can now use java spark

--- src/main/java/RESTful/Application.java
+++ src/main/java/RESTful/Application.java
@@ -1,7 +1,11 @@
 package RESTful;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+
 import static spark.Spark.*;
 
-public class Spark {
+public class Application {
     public static void main(String[] args) {
         get("/hello", (req, res) -> "Hello World");
     }
