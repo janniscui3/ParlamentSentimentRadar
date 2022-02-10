3311cf3442ee8610452482bbec2284738bbe1c9a
TimoE <s7020028@stud.uni-frankfurt.de>
2022-02-11 00:38:29 +0100

    Successfully connected website and database

--- Website/scripts/mainscript.js
+++ Website/scripts/mainscript.js
@@ -3,9 +3,73 @@
 //initializes graphs at the start
 function initiatePage(){ 
     
-    updateTokenLineChart(["Peter","Olaf","Martin"],[13,23,400])
+    getTokens();
+    //updateTokenLineChart(["Peter","Olaf","Martin"],[13,23,400])
     updatePosBarChart(["Peter","Olaf","Martin"],[13,23,400])
     updateSentimentRadarChart([275,215,109])
     updateNamedEntLineChart(["1","2","3","4","5"],[[274,253,192,115],[223,210,130,75],[299,221,132,15,5]]);
     updateSpeakerBarChart(["Peter","Olaf","Martin"],[13,23,400]);
+}
+
+function getTokens(){
+
+    $(function(){
+		$.ajax({
+			method: "GET",
+            dataType: "json",
+			url: "http://localhost:4567/tokens",
+            contentType: "text/plain",
+            			
+            success: function(data){
+                
+
+
+
+
+
+
+                
+                var token_list = data["result"];
+                var token_name = [];
+                var token_amount = [];
+                
+                    
+                for(var i = 0; i < 50; i++){
+                    token_name.push(token_list[i]["token"]);
+                    token_amount.push(token_list[i]["count"]);
+                }
+
+                updateTokenLineChart(token_name, token_amount);
+                
+
+                /*
+                if (data["success"] == true){
+                    var token_list = data["result"];
+                    var token_name = [];
+                    var token_amount = [];
+                    
+                    for(var i = 0; i < 50; i++){
+                        token_name.push(token_list[i]["token"]);
+                        token_amount.push(token_list[i]["count"]);
+                    }
+                    
+                    updateBarChart(token_name,token_amount,chartnumber,mode,newbuttonname);
+                    
+
+
+                  
+                }
+                else{
+                    alert("ERROR");
+                }
+                */
+            },
+            error: function (xhr, ajaxOptions, thrownError){
+                alert(xhr.status)
+                alert(thrownError);
+            }
+        });			
+		
+    });        
+
 }
--- src/main/java/RESTful/Application.java
+++ src/main/java/RESTful/Application.java
@@ -2,7 +2,7 @@ package RESTful;
 
 import org.bson.Document;
 import org.json.JSONObject;
-
+import spark.Filter;
 import java.net.URL;
 import java.net.URLClassLoader;
 
@@ -12,6 +12,30 @@ import static spark.Spark.*;
 public class Application {
     public static void main(String[] args) {
         MongoDBConnectionHandler connectionHandler = new MongoDBConnectionHandler();
+        
+        options("/*",
+        (request, response) -> {
+
+            String accessControlRequestHeaders = request
+                    .headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers",
+                        accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request
+                    .headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods",
+                        accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+        });
 
         get("/tokens", (req, res) -> {
             Document doc = connectionHandler.getDocumentFromCollection("tokens", "statistics");
